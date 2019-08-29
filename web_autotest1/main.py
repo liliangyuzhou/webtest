c8fc8f3997d2ea9a2ba07d1d810c162178f273d5
liliang <1287641566@qq.com>
2019-08-29 17:45:01 +0800

    修改main文件

--- web_autotest1/main.py
+++ web_autotest1/main.py
@@ -8,7 +8,7 @@ logger=my_logger(__name__)
 discover=unittest.defaultTestLoader.discover(constants.tescase_dir,'test_*.py')
 with open(constants.report_dir+'/report.html','wb+') as file:
     runner=HTMLTestRunnerNew.HTMLTestRunner(stream=file, verbosity=2,
-                                            title="测试报告",description="web页面自动化测试",
+                                            title="test-reporter",description="autotest",
                                             tester="liliang")
     runner.run(discover)
 
