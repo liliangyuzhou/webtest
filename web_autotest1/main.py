f12692702765b1231ec94e0c2edeac1f17840b1f
liliang <1287641566@qq.com>
2019-08-29 18:57:48 +0800

    修改main

--- web_autotest1/main.py
+++ web_autotest1/main.py
@@ -1,9 +1,9 @@
 from common import HTMLTestRunnerNew
 import unittest
 from common import constants
-from common.logger import my_logger
 
-logger=my_logger(__name__)
+
+
 
 discover=unittest.defaultTestLoader.discover(constants.tescase_dir,'test_*.py')
 with open(constants.report_dir+'/report.html','wb+') as file:
